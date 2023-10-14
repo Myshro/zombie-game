0638b585a3e5bc345f2504d38981c09a39cade2b
VincentChan22 <74437770+VincentChan22@users.noreply.github.com>
2023-10-14 16:10:55 -0400

    Added gun and shooting
    also added inputmap actions

---
+++ assets/scripts/bullet.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+class_name Bullet
+
+@export var speed :float = 100
+func _ready():
+	print("I WAS BORN")
+func _physics_process(delta):
+	position += transform.x * speed * delta
+
+
+#func _on_Bullet_body_entered(body):
+#	if(body.name == "zombie"):
+#		#zombie takes damage
+#		pass
+#	queue_free()
+#
--- assets/scripts/player.gd
+++ assets/scripts/player.gd
@@ -1,9 +1,16 @@
 extends CharacterBody2D
 
+class_name Player
+
 @export var speed := 100
 @export var acc := 10
+
+
 func _physics_process(_delta):
 	var input_direction := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	velocity.x = move_toward(velocity.x, speed * input_direction.x, acc) 
 	velocity.y = move_toward(velocity.y, speed * input_direction.y, acc) 
+	
+	look_at(get_global_mouse_position())
+	
 	move_and_slide()

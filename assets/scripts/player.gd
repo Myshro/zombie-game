30bee108590b368dc6adda2b0e2ad362f9b74342
Myshro <greenfishfungus2@gmail.com>
2023-10-14 14:31:39 -0400

    zombie chases you

--- assets/scripts/player.gd
+++ assets/scripts/player.gd
@@ -2,8 +2,11 @@ extends CharacterBody2D
 
 @export var speed := 100
 @export var acc := 10
+@export var health_comp : HealthComp
+
 func _physics_process(_delta):
 	var input_direction := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	velocity.x = move_toward(velocity.x, speed * input_direction.x, acc) 
 	velocity.y = move_toward(velocity.y, speed * input_direction.y, acc) 
 	move_and_slide()
+	look_at(get_global_mouse_position())

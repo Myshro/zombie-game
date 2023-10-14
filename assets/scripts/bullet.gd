7031154f16eae3dfc74bd9f4b238b04b41cf280a
VincentChan22 <74437770+VincentChan22@users.noreply.github.com>
2023-10-14 16:39:10 -0400

    gun

--- assets/scripts/bullet.gd
+++ assets/scripts/bullet.gd
@@ -9,9 +9,9 @@ func _physics_process(delta):
 	position += transform.x * speed * delta
 
 
-#func _on_Bullet_body_entered(body):
-#	if(body.name == "zombie"):
-#		#zombie takes damage
-#		pass
-#	queue_free()
-#
+func _on_Bullet_body_entered(body):
+	if(body.name == "zombie"):
+		#zombie takes damage
+		pass
+	queue_free()
+

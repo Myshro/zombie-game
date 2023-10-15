958a6973c400d54d5a90315cc7c92d8699d8f12e
VincentChan22 <74437770+VincentChan22@users.noreply.github.com>
2023-10-14 21:05:22 -0400

    collision

--- assets/scripts/health_comp.gd
+++ assets/scripts/health_comp.gd
@@ -10,6 +10,8 @@ func _ready():
 
 func take_damage(val: int):
 	set_health(health - val)
+	if(health <= 0):
+		die()
 	
 func set_health(val: int):
 	if val > max_health:
@@ -18,3 +20,5 @@ func set_health(val: int):
 		health = 0
 	else:
 		health = val
+func die():
+	get_parent().queue_free()

c25c404957424d99056399e98e0a0c32aad0cd12
VincentChan22 <74437770+VincentChan22@users.noreply.github.com>
2023-10-15 00:55:58 -0400

    fixed bug with  player path
    fixed bug with  player path

--- assets/scripts/zombie.gd
+++ assets/scripts/zombie.gd
@@ -1,6 +1,6 @@
 extends CharacterBody2D
 
-@onready var player := get_tree().root.get_node("TileMap/Player") 
+@onready var player := get_tree().root.get_node("TEST/TileMap/Player") 
 #@export var player : Node2D
 @export var corpse : PackedScene
 @onready var nav_agent := $NavigationAgent2D as NavigationAgent2D
